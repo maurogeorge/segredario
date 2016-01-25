2cfef529723b439a0e172f1df0f943bee8eb5ff9
Mauro George <maurogot@gmail.com>
2016-01-25 20:26:10 -0200

    Extract method on Preload

--- assets/javascripts/Preload.js
+++ assets/javascripts/Preload.js
@@ -4,18 +4,25 @@ Segredario.Preload = function() {};
 Segredario.Preload.prototype = {
 
   preload: function() {
+    this.logo();
+    this.preloadBar();
+  },
+
+  create: function() {
+
+  },
+
+  logo: function() {
     this.logo = this.add.sprite(this.world.centerX,
                                 this.world.centerY, 'logo');
     this.logo.anchor.setTo(0.5);
+  },
 
+  preloadBar: function() {
     this.preloadBar = this.add.sprite(this.game.world.centerX,
                                       this.game.world.centerY, 'preload-bar');
     this.preloadBar.anchor.setTo(0.5, -11);
     this.preloadBar.scale.setTo(7, 2);
     this.load.setPreloadSprite(this.preloadBar);
-  },
-
-  create: function() {
-
   }
 };
