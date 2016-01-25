5ef26e5e768633e0b940797fabad1fb28c36b741
Mauro George <maurogot@gmail.com>
2016-01-25 19:56:15 -0200

    Create Boot state

---
+++ assets/javascripts/Boot.js
@@ -0,0 +1,19 @@
+var Segredario = Segredario || {};
+Segredario.Boot = function() {};
+
+Segredario.Boot.prototype = {
+
+  preload: function() {
+    this.load.image('preload-bar', 'assets/images/preload-bar.png');
+  },
+
+  create: function() {
+    this.game.stage.backgroundColor = '#000';
+
+    this.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+  }
+};
--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,14 +1,6 @@
 var Segredario = Segredario || {};
 
-Segredario.game = new Phaser.Game(800, 600, Phaser.AUTO, '',
-                                  { preload: preload, create: create });
+Segredario.game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 
-function preload () {
-  Segredario.game.load.image('logo', 'assets/images/phaser.png');
-}
-
-function create () {
-  var logo = Segredario.game.add.sprite(Segredario.game.world.centerX,
-                                        Segredario.game.world.centerY, 'logo');
-  logo.anchor.setTo(0.5, 0.5);
-}
+Segredario.game.state.add('Boot', Segredario.Boot);
+Segredario.game.state.start('Boot');
