0dab75a483c98e83dadcae946becfecacae6a789
Mauro George <maurogot@gmail.com>
2016-01-28 19:44:34 -0200

    Add player movement

--- assets/javascripts/game.js
+++ assets/javascripts/game.js
@@ -10,6 +10,14 @@ Segredario.Game.prototype = {
   create: function() {
     this.createLevel();
     this.createPlayer();
+    this.createCursors();
+  },
+
+  update: function(){
+    this.game.physics.arcade.collide(this.player, this.blockedLayer, this.playerHit, null, this);
+
+    this.movePlayer();
+
   },
 
   render: function() {
@@ -17,13 +25,64 @@ Segredario.Game.prototype = {
   },
 
   createPlayer: function() {
-    var player = this.game.add.sprite(10, 176, 'player');
+    this.player = this.game.add.sprite(10, 176, 'player');
+    this.game.physics.arcade.enable(this.player);
+    this.player.body.gravity.y = 300;
+    this.game.camera.follow(this.player);
+  },
+
+  playerResetVelocity: function() {
+    this.player.body.velocity.x = 0;
+  },
+
+  playerStand: function() {
+    this.player.frame = 0;
+  },
+
+  playerMoveLeft: function() {
+    this.player.body.velocity.x = -80;
+  },
+
+  playerMoveRight: function() {
+    this.player.body.velocity.x = 80;
+  },
+
+  playerJump: function() {
+    if(this.player.body.blocked.down) {
+      this.player.body.velocity.y = -200;
+    }
+  },
+
+  movePlayer: function() {
+    this.playerResetVelocity();
+    if (this.cursors.left.isDown) {
+      this.playerMoveLeft();
+    } else if (this.cursors.right.isDown) {
+      this.playerMoveRight();
+    } else {
+      this.playerStand();
+    }
+
+    if (this.cursors.up.isDown) {
+      this.playerJump();
+    }
+  },
+
+  playerHit: function(player, blockedLayer) {
   },
 
   createLevel: function() {
     var level = this.game.add.tilemap('level');
     level.addTilesetImage('super_mario_bros', 'tiles');
-    level.createLayer('backgroundLayer');
-    level.createLayer('blockedLayer');
-  }
+
+    var backgroundLayer = level.createLayer('backgroundLayer');
+    this.blockedLayer = level.createLayer('blockedLayer');
+
+    level.setCollisionBetween(1, 100, true, 'blockedLayer');
+    backgroundLayer.resizeWorld();
+  },
+
+  createCursors: function() {
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+  },
 };
