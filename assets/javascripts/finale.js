9aa3bad4019c6a671246bc527e9b7f163d354f7e
Mauro George <maurogot@gmail.com>
2016-02-04 20:14:34 -0200

    Create finale animation

--- assets/javascripts/finale.js
+++ assets/javascripts/finale.js
@@ -8,7 +8,10 @@ Segredario.Finale.prototype = {
   create: function() {
     this.createLevel();
     this.createJessica();
+    this.createJessicaAnimations();
     this.createMauro();
+    this.createMauroAnimations();
+    this.createAnimations();
   },
 
   createLevel: function() {
@@ -27,6 +30,64 @@ Segredario.Finale.prototype = {
   },
 
   createMauro: function() {
-    this.mauro = this.game.add.sprite(180, 184, 'mauro');
-  }
+    this.mauro = this.game.add.sprite(220, 184, 'mauro');
+  },
+
+  createJessicaAnimations: function(){
+    this.jessica.animations.add('right', [1, 2, 3], 10, true);
+  },
+
+  createMauroAnimations: function(){
+    this.mauro.animations.add('left', [0, 1], 10, true);
+    this.mauro.animations.add('right', [0, 1], 10, true);
+  },
+
+  createAnimations: function() {
+    this.jessica.animations.play('right');
+    this.mauro.animations.play('left');
+
+
+    this.game.add.tween(this.jessica).to({
+      x: 150,
+    },
+    1000, Phaser.Easing.Linear.None, true).onComplete.add(function(){
+      this.jessica.animations.stop();
+      this.jessica.frame = 0;
+    }.bind(this));
+
+    this.game.add.tween(this.mauro).to({
+      x: 180,
+    },
+    1000, Phaser.Easing.Linear.None, true).onComplete.add(function(){
+      this.mauro.animations.stop();
+      this.mauro.frame = 0;
+
+      var goodBye = function() {
+
+        this.jessica.animations.play('right');
+
+        this.game.add.tween(this.jessica).to({
+          x: 170,
+        },
+        500, Phaser.Easing.Linear.None, true).onComplete.add(function(){
+
+          this.mauro.animations.play('right');
+
+          this.game.add.tween(this.jessica).to({
+            x: 260,
+          },
+          1000, Phaser.Easing.Linear.None, true)
+
+            this.game.add.tween(this.mauro).to({
+              x: 260,
+            },
+            1000, Phaser.Easing.Linear.None, true)
+
+        }.bind(this));
+      }.bind(this);
+
+      this.game.time.events.add(Phaser.Timer.SECOND * 2, goodBye, this);
+
+    }.bind(this));
+  },
 };
